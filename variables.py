1bc3e41849a0c291c8358e19c7d9965dfce8449b
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-08-20 15:48:44 +0100

    variables

---
+++ variables.py
@@ -0,0 +1,15 @@
+name = "Mike"
+age = 25
+height = "1.37"
+
+print(name)
+print(age)
+print(height)
+
+##################
+
+server_name = "web-01"
+cpu_cores = 2
+is_online = True
+
+print(f"{server_name} has {cpu_cores} cpu cores. Online status: {is_online}")
