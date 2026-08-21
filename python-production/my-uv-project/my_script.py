4f3d77fb7552d267e207cba55e411db80c235d89
Unais Rashid <unais@Unaiss-MacBook-Pro.local>
2026-08-21 12:29:52 +0100

    uv learning

---
+++ python-production/my-uv-project/my_script.py
@@ -0,0 +1,5 @@
+colours = ["red", "blue", "green"]
+print(colours[0])
+print(colours[2])
+colours.append("yellow")
+print(colours)
---
+++ python-production/uv.py
@@ -0,0 +1,6 @@
+# virtual env is seperate private toolbox of python packages for one project
+# uv creates environments and installs packages also manages list of dependencies required for project
+
+# create a new project folder with uv
+# uv init my-project
+# cd my-project
